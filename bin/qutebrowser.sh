d4d80c895aa6c2ec362bcb7c9370b3be7eea86b2
Alexander Sherikov <alexander@sherikov.net>
2022-05-29 15:04:40 +0200

    Fix fluxbox startup script

--- bin/qutebrowser.sh
+++ bin/qutebrowser.sh
@@ -3,6 +3,7 @@
 export LANG=en_EN.UTF-8
 
 /usr/local/bin/qutebrowser $@
+#${HOME}/.local/qutebrowser/.venv/bin/python3 -m qutebrowser "$@"
 
 # https://bugreports.qt.io/browse/QTBUG-62957
 rm -Rf /home/aleks/VideoDecodeStats
