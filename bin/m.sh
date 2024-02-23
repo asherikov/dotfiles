00b1da5a87a3576c51c5930db045d9849c443b55
Alexander Sherikov <alexander@sherikov.net>
2024-02-23 16:07:26 +0400

    Update/add X11/Wayland configs

--- bin/m.sh
+++ bin/m.sh
@@ -3,9 +3,7 @@
 case $1 in
   g) getmail -r get${2}; date; exit 0;;
   s) mutt -F ~/.config/mutt/${2}; exit 0;;
-  rss)
-      rawdog -u; date;
-      exit 0;;
+  rss) r2e run; date; exit 0;;
   n) newsfetch news.karelia.ru -p procmail; muttng -y; exit 0;;
   *) mutt -y; exit 0;;
 esac
